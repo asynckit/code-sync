6d9425da10b1035530ac13e201662b5884b3373d
Ankit <akc9065@gmail.com>
2023-01-02 22:13:38 +0530

    Styled the design so far, now design editor 1:31:18

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return (
-    <div>EditorPage</div>
-  )
-}
+    const [clients, setClients] = useState([
+        { socketId: 1, username: "Ankit k" },
+        { socketId: 2, username: "Capt. jack" },
+        { socketId: 3, username: "Capt. jack" },
+    ]);
+    return (
+        <div className="mainWrap">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img
+                            className="logoImage"
+                            src="/code-sync.png"
+                            alt="logo"
+                        />
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientsList">
+                        {clients.map((client) => (
+                            <Client
+                                key={client.socketId}
+                                username={client.username}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <button className="btn copyBtn">Copy RoomId</button>
+                <button className="btn leaveBtn">Leave</button>
+            </div>
+            <div className="editorWrap">
+              <Editor />
+            </div>
+        </div>
+    );
+};
 
-export default EditorPage
+export default EditorPage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,87 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-  return (
-    <div className="homePageWrapper">
-      <div className="formWrapper">
-        <img className="homePageLogo" src='/code-sync.png' alt="code-sync-logo" />
-        <h4 className="mainLabel">Please paste your room invitation ID</h4>
-        <div className="inputGroup">
-          <input type="text" className="inputBox" placeholder='Room Id'></input>
-          <input type="text" className="inputBox" placeholder='Username'></input>
-          <button className="btn joinBtn">Join</button>
-          <span className="createInfo">
-            if you don't have an invite then create &nbsp;
-            <a href="" className='createNewBtn'>new room</a>
-          </span>
-        </div>
-      </div>
-      <footer>
-        <h4>Built with ❤️ with <a href="www.google.com">Ankit k</a></h4>
-      </footer>
-    </div>
-  )
-}
+    const navigate = useNavigate();
+    const [roomId, setRoomId] = useState("");
+    const [username, setUsername] = useState("");
 
-export default Home
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidv4();
+        setRoomId(id);
+        toast.success("New Room Created!");
+    };
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            toast.error("Room id and Username is required!");
+            return;
+        }
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
+    const handleInputEnter = (e)=>{
+      console.log('event', e.code);
+      if(e.code === 'Enter'){
+        joinRoom();
+      }
+    }
+    return (
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img
+                    className="homePageLogo"
+                    src="/code-sync.png"
+                    alt="code-sync-logo"
+                />
+                <h4 className="mainLabel">
+                    Please paste your room invitation ID
+                </h4>
+                <div className="inputGroup">
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="Room Id"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    ></input>
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
+
+                    ></input>
+                    <button className="btn joinBtn" onClick={joinRoom}>
+                        Join
+                    </button>
+                    <span className="createInfo">
+                        if you don't have an invite then create &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
+                            new room
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <footer>
+                <h4>
+                    Built with ❤️ with <a href="www.google.com">Ankit k</a>
+                </h4>
+            </footer>
+        </div>
+    );
+};
+
+export default Home;
